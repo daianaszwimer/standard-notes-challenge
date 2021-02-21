0cde40a5bfd8e76814c9ce77b336c4423b086531
Daiana K Szwimer <d.szwimer@distillery.com>
2021-02-21 10:37:41 -0300

    create subtitle component

---
+++ src/modules/common/components/Subtitle/Subtitle.js
@@ -0,0 +1,9 @@
+import "./Subtitle.css";
+
+const Subtitle = ({ text }) => (
+  <h2 className="subtitle">
+    {text}
+  </h2>
+);
+
+export default Subtitle;
---
+++ src/modules/common/components/Subtitle/index.js
@@ -0,0 +1,3 @@
+import Subtitle from "./Subtitle.js";
+
+export default Subtitle;
