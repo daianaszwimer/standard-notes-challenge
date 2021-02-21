8cccad22b386066946915dded3cd0fcd7a13c13c
Daiana K Szwimer <d.szwimer@distillery.com>
2021-02-21 11:40:35 -0300

    show link component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Title from "./modules/common/components/Title";
 import Subtitle from "./modules/common/components/Subtitle";
+import Link from "./modules/common/components/Link";
 
 const TITLE_TEXT = "An all-in-one personal knowledge base";
 const SUBTITLE_TEXT = "Write simple notes, or complex ones. From tasks and todos, to spreadsheets, markdown, and rich text, you can store all types of information quickly and securely in Standard Notes.";
+const LINK_TEXT = "Explore our features";
 
 const App = () => (
   <div className="wrapper">
@@ -11,6 +13,7 @@ const App = () => (
     <div className="subtitle-wrapper">
       <Subtitle text={SUBTITLE_TEXT}/>
     </div>
+    <Link text={LINK_TEXT} url="#" shouldShowIcon/>
   </div>
 );
 
