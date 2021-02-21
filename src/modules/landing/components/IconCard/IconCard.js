0250827dbff443addb6c8fdaa4f3aca2aa1531b4
Daiana K Szwimer <d.szwimer@distillery.com>
2021-02-21 15:17:57 -0300

    create icon card component

---
+++ src/modules/landing/components/IconCard/IconCard.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./IconCard.css";
+import Icon from "../Icon";
+import { DEFAULT, COLORED } from "../Icon/states";
+
+const IconCard = ({ text, id, hoverIconColor }) => {
+  const [ isHovering, setIsHovering ] = useState(false);
+
+  const handleOnMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleOnMouseLeave = () => {
+    setIsHovering(false);
+  };
+
+  return (
+    <div className="icon-card-wrapper" onMouseEnter={handleOnMouseEnter} onMouseLeave={handleOnMouseLeave}>
+      <Icon
+        id={id}
+        state={isHovering ? COLORED : DEFAULT}
+        backgroundColor={hoverIconColor}
+      />
+      <span
+        className="icon-card-text"
+      >
+        {text}
+      </span>
+    </div>
+  );
+};
+
+export default IconCard;
