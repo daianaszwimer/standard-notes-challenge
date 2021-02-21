d3509c4de98ee54f2ce343d3093ed1f5025aeca8
Daiana K Szwimer <d.szwimer@distillery.com>
2021-02-21 00:55:23 -0300

    use flex and use arrow function syntax for app component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import "./App.css";
 import Header from "./modules/common/components/Header";
 
 const HEADER_TEXT = "An all-in-one personal knowledge base";
-function App() {
-  return (
-    <div className="app">
-      <Header text={HEADER_TEXT}/>
-    </div>
-  );
-}
+
+const App = () => (
+  <div className="wrapper">
+    <Header text={HEADER_TEXT}/>
+  </div>
+);
 
 export default App;
