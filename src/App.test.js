e614848bb2afc6ac5304ad7d3c903cde5cf8a58d
Daiana K Szwimer <d.szwimer@distillery.com>
2021-02-22 01:46:30 -0300

    unit test: test that title, subtitle and link text renders

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import {
+  TITLE_TEXT,
+  SUBTITLE_TEXT,
+  LINK_TEXT
+} from "./modules/landing/constants";
 
-test("renders learn react link", () => {
+test("renders title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(TITLE_TEXT);
+  expect(linkElement).toBeInTheDocument();
+});
+test("renders subtitle", () => {
+  render(<App />);
+  const linkElement = screen.getByText(SUBTITLE_TEXT);
+  expect(linkElement).toBeInTheDocument();
+});
+test("renders link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(LINK_TEXT);
   expect(linkElement).toBeInTheDocument();
 });
