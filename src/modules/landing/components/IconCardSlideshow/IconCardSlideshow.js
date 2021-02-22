0f4f793c557ad64ee88ab03adc3954520ab2ae15
Daiana K Szwimer <d.szwimer@distillery.com>
2021-02-22 00:42:02 -0300

    fix slideshow for big resolutions

--- src/modules/landing/components/IconCardSlideshow/IconCardSlideshow.js
+++ src/modules/landing/components/IconCardSlideshow/IconCardSlideshow.js
@@ -4,8 +4,8 @@ import IconCard from "../IconCard";
 const IconCardSlideshow = ({ cards }) => (
   <>
     <ul className="icon-card-slideshow-wrapper">
-      {/* we use the same array with items duplicated because otherwise in some resolutions there is a space that makes the animation looks weird */}
-      {cards.concat(cards).map((card, index) =>
+      {/* we use the same array with items duplicated because otherwise in big resolutions there is a space that makes the animation looks bad */}
+      {cards.concat(cards).concat(cards).map((card, index) =>
         <li
           key={`${card.id}-${index}`}
           className="icon-card-slideshow-item"
